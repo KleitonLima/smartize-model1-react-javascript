f828142abfd32c8a032c5272f5edcc1c48c06d7a
Kleiton Lima <kleiton.mini@gmail.com>
2022-06-02 23:02:46 -0300

    Create: Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
 
-function App() {
+const App = () => {
   return (
     <div>
-      
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import logo from "../../assets/images/logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <img src={logo} width="150px" alt="Logo Smartize" />
+    </header>
+  );
+};
+
+export default Header;
