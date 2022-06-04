dbdcadf6b1a61104be0dc33ea633be2fb72e437c
Kleiton Lima <kleiton.mini@gmail.com>
2022-06-03 22:09:25 -0300

    Create: Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,14 @@
 import logo from "../../assets/images/logo.png";
+import "./style.css";
 
 const Header = () => {
   return (
     <header>
       <img src={logo} width="150px" alt="Logo Smartize" />
+      <div>
+        <input type="text" placeholder="Digite o tipo do produto" />
+        <button>O</button>
+      </div>
     </header>
   );
 };
