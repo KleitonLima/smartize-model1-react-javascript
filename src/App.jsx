da6bbe648e2ce5c57b6a26e0d518df33be50a400
Kleiton Lima <kleiton.mini@gmail.com>
2022-06-04 11:17:22 -0300

    Create: Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 };
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+
+const Home = () => {
+  return (
+    <div>
+      <div id="productResearched">
+        <h1>Produto encontrado</h1>
+      </div>
+      <div id="allProducts">Produtos disponíveis</div>
+    </div>
+  );
+};
+
+export default Home;
