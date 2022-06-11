d4f1d05ea53fb817664655f7c0e199af3bed884e
Kleiton Lima <kleiton.mini@gmail.com>
2022-06-11 12:00:53 -0300

    Remove: input search

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 
 const Header = () => {
   const [showModalCadastre, setShowModalCadastre] = useState(false);
-  
+
   const handleShowModalCadastre = () => {
     return setShowModalCadastre(!showModalCadastre);
   };
@@ -15,10 +15,6 @@ const Header = () => {
     <>
       <header>
         <img src={logo} width="150px" alt="Logo Smartize" />
-        {/* <div>
-        <input type="text" placeholder="Digite o tipo do produto" />
-        <button>O</button>
-      </div> */}
         <div>
           <i onClick={() => toast.error("Sacola em desenvolvimento...")} className="fa-solid fa-bag-shopping"></i>
           <i onClick={handleShowModalCadastre} className="fa-solid fa-square-plus"></i>
