ae079247d377cf3d4c2ae3d9701d210e840a5145
Kleiton Lima <kleiton.mini@gmail.com>
2022-06-11 14:57:04 -0300

    Create: icons edit and delete

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,12 +4,16 @@ const Card = ({ products }) => {
   return (
     <div className="card">
       <img src={products.foto} alt={`Foto de ${products.modelo}`} />
-      <div>
+      <div className="cardInfos">
         <h3>{products.tipo}</h3>
         <h3>{products.marca}</h3>
         <h3>{products.modelo}</h3>
         <h3>{products.condicao}</h3>
         <h2>{`R$${products.preco.toFixed(2)}`}</h2>
+        <div>
+          <i className="fa-solid fa-pen-to-square"></i>
+          <i className="fa-solid fa-trash"></i>
+        </div>
       </div>
     </div>
   );
