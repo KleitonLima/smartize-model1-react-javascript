ce9b6b9d43d5388da6e7aced4822165154cdec7c
Kleiton Lima <kleiton.mini@gmail.com>
2022-06-13 08:58:57 -0300

    style: named classes

--- src/components/Modals/ModalDeleteProduct/index.jsx
+++ src/components/Modals/ModalDeleteProduct/index.jsx
@@ -2,12 +2,12 @@ import "./style.css";
 
 const MoldaDeleteProduct = () => {
   return (
-    <div>
-      <div>
-        <div>
+    <div className="modal-background">
+      <div className="modalDelete-container">
+        <div className="modalDelete-header">
           <h2>Deseja excluir o produto {product.tipo}?</h2>
         </div>
-        <div>
+        <div className="modalDelete-actions">
           <button onClick={() => {}}>SIM</button>
           <button>NÃO</button>
         </div>
