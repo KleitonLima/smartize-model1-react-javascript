0148ee148cae2098f1d3b27fd8626a9783501ee9
Kleiton Lima <kleiton.mini@gmail.com>
2022-06-13 23:24:54 -0300

    fix: url online

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:3001" });
+const api = axios.create({ baseURL: "https://api-loja-smartize.herokuapp.com" });
 
 export default api;
