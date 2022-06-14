6dea2a347916ee30ea8c1556355b9767fb0e30f5
Kleiton Lima <kleiton.mini@gmail.com>
2022-06-13 23:53:53 -0300

    fix: url api

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "https://api-loja-smartize.herokuapp.com" });
+const api = axios.create({ baseURL: "https://api-loja-smartize.herokuapp.com/" });
 
 export default api;
